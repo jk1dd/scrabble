64e4acbefe728e96d8c553e8c57627d6391d7f00
jk1dd <portajonk@gmail.com>
2017-02-17 12:08:17 -0700

    add functionality for scrabble game

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -17,4 +17,58 @@ class Scrabble
       "Y"=>4, "Z"=>10
     }
   end
+
+  def score_with_multipliers(word, times_letter_scored, word_multiplier)
+    score_array = word.upcase.chars.map do |letter|
+      point_values[letter]
+    end
+    total_score = score_array.zip(times_letter_scored).map { |x, y| x * y }.reduce(:+)
+    if word.length >= 7
+      (total_score + 10) * word_multiplier
+    else
+      total_score * word_multiplier
+    end
+  end
+
+  def highest_scoring_word(word_list)
+    # scores = word_list.map do |word|
+    #   score(word)
+    # end
+    # words_with_scores = word_list.zip(scores)
+    # words_with_scores.max_by {|word, score| score}[0]
+    scores = {}
+    word_list.each do |word|
+      scores[word] = score(word)
+    end
+    greatest_values = scores.max_by {|key, value| value}[1]
+    highest_scores = []
+    scores.each do |key, value|
+      if value == greatest_values
+        highest_scores << key
+      end
+    end
+    highest_scores
+    highest_scores.each do |word|
+      if word.length == 7
+        return word
+      end
+    end
+    word_lengths = []
+    highest_scores.each do |word|
+      word_lengths << word.length
+    end
+    highest_scores.each do |word|
+      if word_lengths.min == word.length
+        return word
+      end
+    end
+
+
+
+    # binding.pry
+    # words_with_scores.find do |pair|
+    #   pair[1].max
+    # end
+  end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest'
 require_relative '../lib/scrabble'
 require 'minitest/autorun'
-require 'minitest/emoji'
+require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
@@ -9,4 +9,33 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_can_score_multi_letters
+    assert_equal 9, Scrabble.new.score_with_multipliers('hello', [1,2,1,1,1], 1)
+  end
+
+  def test_it_can_score_multi_letters_with_multiplier
+    assert_equal 18, Scrabble.new.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end
+
+  def test_it_can_score_multi_letters_with_multiplier_and_add_bonus
+    assert_equal 58, Scrabble.new.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+  end
+
+  def test_it_can_pick_highest_scoring_word
+    assert_equal "home", Scrabble.new.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+  end
+
+  def test_it_can_pick_highest_scoring_word_with_fewest_letters
+    assert_equal "word", Scrabble.new.highest_scoring_word(['hello', 'word', 'sound'])
+  end
+
+  def test_it_can_pick_highest_scoring_word_with_seven_letters
+    assert_equal "silence", Scrabble.new.highest_scoring_word(['home', 'word', 'silence'])
+  end
+
+  def test_it_will_pick_the_first_listed_if_words_have_same_score_and_length
+    assert_equal "word", Scrabble.new.highest_scoring_word(['hi', 'word', 'ward'])
+  end
+
 end
